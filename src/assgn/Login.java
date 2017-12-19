ef9eb29e7c2880f92a692783191ec762e528cbee
Mixed_97 <Mixed_97@MiXedkun>
2017-12-19 18:02:27 +0800

    Merge origin/master
    Conflicts:
    	src/listLink/store.java

--- src/assgn/Login.java
+++ src/assgn/Login.java
@@ -238,6 +238,9 @@ public class Login extends javax.swing.JFrame {
             public void run() {
                 new Login().setVisible(true);
                 
+                
+                
+                
             }
         });
     }
