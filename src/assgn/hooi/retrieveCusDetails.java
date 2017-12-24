ef5a79d6b05fe695f18eff19f597b9a8a2d9d7fa
Aphro97 <33274725+Aphro97@users.noreply.github.com>
2017-12-24 21:26:31 +0800

    Merge origin/master
    Conflicts:
    	src/assgn/Login.java
    	src/listLink/store.java

---
+++ src/assgn/hooi/retrieveCusDetails.java
@@ -0,0 +1,143 @@
+
+package assgn.hooi;
+
+import assgn.Customer;
+import javax.swing.JOptionPane;
+import listLink.ListLink;
+import listLink.ListLinkInt;
+import listLink.store;
+
+/**
+ *
+ * @author Aphro97
+ */
+public class retrieveCusDetails extends javax.swing.JFrame {
+    store save;
+    ListLinkInt<Customer> customer = new ListLink<>();
+    Customer c;
+
+    public retrieveCusDetails() {
+        initComponents();
+        save = new store(1);
+        setup();
+    }
+    public retrieveCusDetails(store save) {
+        initComponents();
+        this.save = save;
+        setup();
+    }
+    private void setup(){
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Retrieve Customer Details");
+        customer = save.getCustomer();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        phoneTxt = new javax.swing.JTextField();
+        searchBtn = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        searchBtn.setText("Search");
+        searchBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchBtnActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Enter customer's phone number");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(57, 57, 57)
+                        .addComponent(phoneTxt))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel1)))
+                .addGap(32, 32, 32)
+                .addComponent(searchBtn)
+                .addGap(29, 29, 29))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(phoneTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(searchBtn))
+                .addContainerGap(49, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void searchBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchBtnActionPerformed
+        // search customer details
+        String phone = phoneTxt.getText();
+        for(int a=1;a<customer.getSize()+1;a++){
+            c = customer.get(a);
+            System.out.println(customer.getSize());
+            if(c.getContactNo().equals(phone)){
+                this.setVisible(false);
+                showCusDetails scd = new showCusDetails(save,a);
+                break;
+            }else if(a == customer.getSize()){
+                JOptionPane.showMessageDialog(this, "Unable to find customer with the number "+phoneTxt.getText(), "Searching Customer Details", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            }
+        }
+    }//GEN-LAST:event_searchBtnActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(retrieveCusDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(retrieveCusDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(retrieveCusDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(retrieveCusDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new retrieveCusDetails().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField phoneTxt;
+    private javax.swing.JButton searchBtn;
+    // End of variables declaration//GEN-END:variables
+}
